131ff83a25b31bef381dc72945cd0aae6561d72b
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-19 22:06:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacaoTratExcec/Entities/Exceptions/DomainExcption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacaoTratExcec.Entities.Exceptions
+{
+    class DomainExcption : ApplicationException
+    {
+        public DomainExcption(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ ExercicioFixacaoTratExcec/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using ExercicioFixacaoTratExcec.Entities;
+using ExercicioFixacaoTratExcec.Entities.Exceptions;
+
+namespace ExercicioFixacaoTratExcec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string name = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, name, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainExcption e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
